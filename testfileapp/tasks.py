36db09078f98c575fd2ea905273c72b6bbff9f11
DrowLegend <chernikovao38@gmail.com>
2019-12-22 23:39:35 +0200

    change time task

--- testfileapp/tasks.py
+++ testfileapp/tasks.py
@@ -6,7 +6,7 @@ import time
 @shared_task
 def set_status_as_inactive(pk):
     file = UploadFile.objects.get(pk=pk)
-    time.sleep(5)
+    time.sleep(3)
     file.is_active = True
     file.save()
 
